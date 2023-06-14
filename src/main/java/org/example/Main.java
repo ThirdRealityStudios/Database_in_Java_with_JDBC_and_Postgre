25018ae59bd2b5f32791d297037bde82161b1429
Janek <janek.piekarski.postfach@gmail.com>
2023-06-14 15:04:51 +0200

    You can now enter SQL statements by the console

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,16 @@
 package org.example;
 
+import org.postgresql.util.PSQLException;
+
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)
     {
-        String connectionString = "jdbc:postgresql://yourdatabase.com/yourTable";
+        String connectionString = "jdbc:postgresql://yourdomain.de/databaseName";
         String user = "yourUsername";
         String password = "yourPassword";
-
 
         try
         {
@@ -16,15 +18,56 @@ public class Main {
 
             Statement stmt = con.createStatement();
 
-            ResultSet rs = stmt.executeQuery("SELECT * FROM personen");
+            Scanner s = new Scanner(System.in);
 
-            // while (rs.next())
+            String inputSQL = "";
+
+            while(!inputSQL.equals("/exit"))
             {
-                System.out.printf("%d %s %s (%s)%n",
-                        rs.getInt(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
+                System.out.print("Your SQL statement: ");
+                inputSQL = s.nextLine();
+
+                ResultSet rs = null;
+
+                try
+                {
+                    if(inputSQL.startsWith("SELECT"))
+                    {
+                        rs = stmt.executeQuery(inputSQL);
+                    }
+                    else
+                    {
+                        stmt.execute(inputSQL);
+                        System.out.println("Executed!");
+
+                        continue;
+                    }
+
+                    while(rs.next())
+                    {
+                        ResultSetMetaData rsmd = rs.getMetaData();
+
+                        int results = rsmd.getColumnCount();
+
+                        for(int i = 0; i < results; i++)
+                        {
+                            Object value = rs.getObject(i + 1);
+
+                            System.out.printf("%20s", value.toString());
+
+                            // Etwas Abstand zwischen jeder Ausgabe eines Attributs.
+                            if(i < (results - 1))
+                                System.out.print("    ");
+                        }
+
+                        System.out.println();
+                    }
+                }
+                catch(SQLException eSQL)
+                {
+                    eSQL.printStackTrace();
+                    System.out.println("\nDu haben falsche Syntax oder ich nichts wissen was falsche hier\n");
+                }
             }
         }
         catch(Exception e)
