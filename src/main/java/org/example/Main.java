1c899f2e3422c89f005bbcf0834dc5203b8ec939
Janek <janek.piekarski.postfach@gmail.com>
2023-06-14 11:31:04 +0200

    JDBC Implementation with pom.xml for PostgreSQL

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,37 @@
 package org.example;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world! ");
+	    String dbsUsed = "postgresql";
+	    String dbTarget = "yourdatabase.com/yourTable";
+
+        String connectionString = "jdbc:" + dbsUsed + "://" + dbTarget;
+        String user = "yourUsername";
+        String password = "yourPassword";
+
+        try
+        {
+            Connection con = DriverManager.getConnection(connectionString, user, password);
+
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM personen");
+
+            while (rs.next())
+            {
+                System.out.printf("%d %s %s (%s)%n",
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4));
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
